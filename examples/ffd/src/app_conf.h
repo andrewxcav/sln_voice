23417c333ce44bd9ef33ac79a39ae551f6e736c2
Andrew Cavanaugh <andrewc@xmos.com>
2025-07-25 15:01:21 -0400

    working demo on sq66

--- examples/ffd/src/app_conf.h
+++ examples/ffd/src/app_conf.h
@@ -36,7 +36,7 @@
 
 /* Enable audio response output */
 #ifndef appconfAUDIO_PLAYBACK_ENABLED
-#define appconfAUDIO_PLAYBACK_ENABLED           0
+#define appconfAUDIO_PLAYBACK_ENABLED           1
 #endif
 
 /* Intent Engine Configuration */
